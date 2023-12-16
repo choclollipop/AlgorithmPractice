86cea2a7b4c7aec27b9fd581d26c66799bd7c237
choclollipop <2924733894@qq.com>
2023-12-16 10:11:02 +0800

    [修改] 优化翻转代码，原地进行翻转

--- reverse/revDoubleArray.c
+++ reverse/revDoubleArray.c
@@ -4,11 +4,14 @@
 #define ROW     3
 #define COLUMN  3
 
+/* 将二维数组的行列交换 */
 int main()
 {
     int num[ROW][COLUMN] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int reverse[ROW][COLUMN];
-    memset(reverse, 0, sizeof(reverse));
+    short col = 0;
+    short row = 0;
+    short count = 0;
+    int tmpNum = 0;
 
     /* 打印二维数组 */
     for(int idx = 0; idx < ROW; idx++)
@@ -21,20 +24,29 @@ int main()
     }
 
     /* 翻转数组 */
-    for(int idx = 0; idx < ROW; idx++)
+    /* 原地翻转 */
+    while(count < ROW)
     {
-        for(int pos = 0; pos < COLUMN; pos++)
+        tmpNum = num[row][col];
+        num[row][col] = num[col][row];
+        num[col][row] = tmpNum;
+        col++;
+        if(col == COLUMN)
         {
-            reverse[idx][pos] = num[pos][idx];
+            count++;
+            row = count;
+            col = count;
         }
+       
     }
+
 
     /* 打印翻转后的数组 */
     for(int idx = 0; idx < ROW; idx++)
     {
         for(int pos = 0; pos < COLUMN; pos++)
         {
-            printf("reverse[%d][%d]:%d\t", idx, pos, reverse[idx][pos]);
+            printf("reverse[%d][%d]:%d\t", idx, pos, num[idx][pos]);
         }
         printf("\n");
     }
