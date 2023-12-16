0ff018e71922bbd86842b35f073c34050e9c211c
choclollipop <2924733894@qq.com>
2023-12-16 11:47:18 +0800

    [修改] 将代码修改为原地算法

--- reverse/reverseStr.c
+++ reverse/reverseStr.c
@@ -5,58 +5,45 @@
 #define BUFFER_SIZE 15
 
 /* 将字符串中的单词翻转过来 */
+/* 原地翻转 */
+
+/* 翻转字符换函数 */
+void reverseStr(char * str, int start, int end)
+{
+    int tmp = 0;
+    end = end - 1;
+    while(start <= end)
+    {
+        tmp = str[start];
+        str[start] = str[end];
+        str[end] = tmp;
+        start++;
+        end--;
+    }
+}
+
 int main()
 {
-    char * str = "I am from china";
-    char reverse[BUFFER_SIZE + 1];
-    char * result = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    memset(reverse, 0, sizeof(reverse));
+    char str[] = "I am from china";
+    int start = 0;
     int len = strlen(str);
-    int pos = 0;
-    int tmp = 0;
-    
-    /* 先将原始字符串翻转 */
-    for(int idx = BUFFER_SIZE; idx >= 0; idx--)
-    {
-        reverse[pos] = str[idx];
-        pos++;
-    }
 
-    pos = 0;
+    len = strlen(str);
 
-    for(int idx = 1  ; idx <= len; idx++)
+    /* 翻转单词内部字母顺序 */
+    for(int end = 0; end <= len; end++)
     {
-        if(reverse[idx] == ' ' || idx == len)
+        if(end == len || str[end] == ' ')
         {
-            /* 若此时idx走到最后，则直接将最后一个字符赋值给结果数组即可,并退出 */
-            if(idx == len)
-            {
-                result[pos] = reverse[idx];
-                break;
-            }
-            /* 没有走到最后的时候，进行按位反向赋值 */
-            int tmp1 =idx;
-            for(int sub = pos; sub <= idx - 2; sub++)
-            {
-                result[sub] = reverse[tmp1 - 1];
-                tmp1--;
-                tmp++;
-            }
-
-            pos = idx - 1;
-
-            /* 没有走到最后的时候，需要给每个单词中间加空格*/
-            if(tmp != len)
-            {
-                result[pos] = 32;
-                pos++;
-                tmp++;
-            }
-            
+            reverseStr(str, start, end);
+            start = end + 1;
         }
     }
 
-    printf("result: %s\n", result);
+    /* 翻转整个字符串 */
+    reverseStr(str, 0, len);
+
+    printf("%s\n", str);
 
     return 0;
 }
